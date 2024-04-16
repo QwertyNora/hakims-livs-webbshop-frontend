1872fd876316a2e99cfd044d64b3278146c53e69
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-16 17:18:20 +0200

    Checkout layout, cart table with functionality

--- client/src/pages/checkout.jsx
+++ client/src/pages/checkout.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Breadcrumb, Layout, Menu, theme, Row, Col } from "antd";
+import Cart from "../components/cart";
+import CartItems from "../components/cartItems";
+
 const { Header, Content, Footer } = Layout;
 const items = new Array(15).fill(null).map((_, index) => ({
   key: index + 1,
@@ -20,30 +23,71 @@ function Checkout() {
       >
         <div className="demo-logo" />
       </Header>
-      <Content
+      <Breadcrumb
         style={{
-          padding: "0 48px",
+          margin: "16px 0",
         }}
       >
-        <Breadcrumb
-          style={{
-            margin: "16px 0",
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Checkout</Breadcrumb.Item>
+      </Breadcrumb>
+      <Row
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
+        <Col
+          xs={{
+            span: 5,
+            offset: 0.5,
+          }}
+          lg={{
+            span: 15,
+            offset: 0.5,
           }}
         >
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>Checkout</Breadcrumb.Item>
-        </Breadcrumb>
-        <div
-          style={{
-            background: colorBgContainer,
-            minHeight: 280,
-            padding: 24,
-            borderRadius: borderRadiusLG,
+          <Content
+            style={{
+              padding: "0 48px",
+            }}
+          >
+            <div
+              style={{
+                background: colorBgContainer,
+                minHeight: 280,
+                padding: 24,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              <h2>Your cart</h2>
+              <CartItems />
+            </div>
+          </Content>
+        </Col>
+        <Col
+          xs={{
+            span: 6,
+            offset: 0.5,
+          }}
+          lg={{
+            span: 8,
+            offset: 0.5,
           }}
         >
-          Content
-        </div>
-      </Content>
+          <div
+            style={{
+              background: colorBgContainer,
+              minHeight: 280,
+              padding: 24,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Order
+          </div>
+        </Col>
+      </Row>
+
       <Footer
         style={{
           textAlign: "center",
