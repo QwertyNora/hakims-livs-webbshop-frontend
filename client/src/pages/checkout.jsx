d37f62318b62bcaf26e426c6e04483fb43c52626
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-16 13:22:44 +0200

    Checkout boilerplate

--- client/src/pages/checkout.jsx
+++ client/src/pages/checkout.jsx
@@ -1,8 +1,57 @@
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+const { Header, Content, Footer } = Layout;
+const items = new Array(15).fill(null).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
+
 function Checkout() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
-    <div>
-      <h1 className="page-hero">Checkout</h1>
-    </div>
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+      </Header>
+      <Content
+        style={{
+          padding: "0 48px",
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Checkout</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Hakims livs webbshop ©{new Date().getFullYear()} Created by Grupp 1
+      </Footer>
+    </Layout>
   );
 }
 
