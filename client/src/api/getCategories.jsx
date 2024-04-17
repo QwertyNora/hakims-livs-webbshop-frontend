f174fc7bcd85e00e186767f79d87ef913f99795d
Victor98k <Victorgustav98@gmail.com>
2024-04-17 20:48:59 +0200

    Categories nav done. With the correct structure.

---
+++ client/src/api/getCategories.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Styles from "../styles/home.module.css";
+
+function DisplayCategories({ onCategorySelect }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/categories")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.error("Error fetching categories:", error));
+  }, []);
+
+  const handleCategorySelect = (categoryId, event) => {
+    event.preventDefault();
+    onCategorySelect(categoryId);
+  };
+
+  return (
+    <div className={`${Styles.categoryContainer} ${Styles.SideNav}`}>
+      <h2 className={Styles.categoriesHeader}>Categories</h2>
+      <ul className={Styles.categoryList}>
+        <li>
+          {/* Change the button to a link */}
+          <a
+            href="#"
+            className={Styles.categoryLink}
+            onClick={(e) => handleCategorySelect("", e)}
+          >
+            All
+          </a>
+        </li>
+        {categories.map((category) => (
+          <li key={category._id}>
+            {/* Change the button to a link */}
+            <a
+              href="#"
+              className={Styles.categoryLink}
+              onClick={(e) => handleCategorySelect(category._id, e)}
+            >
+              {category.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DisplayCategories;
